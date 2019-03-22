4216f815f83286deffcf72f2dcf8cab96671d6f7
Sota Suzuki <k017c1356@it-neec.jp>
2019-03-22 13:41:44 +0900

    #97 @2h [add] 掲示板の雛形を作成

---
+++ build/precache-manifest.a8cf6eded5d511490c72f2dce8ecf4c7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7779dfcb15e9e0d1928",
+    "url": "/static/js/main.e359a4f5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "99b8557de023604f4795",
+    "url": "/static/js/2.813d3618.chunk.js"
+  },
+  {
+    "revision": "58a48eb69eba36175951665e3818b29e",
+    "url": "/index.html"
+  }
+];
