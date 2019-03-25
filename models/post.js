33b7f68a1511ee9e5e8f4a2d67d6a2b2a9e7d0fd
Akira Shinohara <k017c1067@it-neec.jp>
2019-03-25 10:46:25 +0900

    [new] #98 @0.1 postモデルの作成

---
+++ models/post.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: String,
+    content: String,
+    author: String,
+});
+
+mongoose.model('Post', PostSchema);
