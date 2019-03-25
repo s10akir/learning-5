1627763903576fc807ed5a4cd8b933e831d6d5b6
Akira Shinohara <k017c1067@it-neec.jp>
2019-03-25 13:34:54 +0900

    [clean]

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cookieParser = require('cookie-parser');
 const express = require('express');
 const httpErrors = require('http-errors');
 const logger = require('morgan');
-const path = require('path');
 
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
@@ -21,18 +20,18 @@ app.use('/api/', apiRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(httpErrors(404));
+    next(httpErrors(404));
 });
 
 // error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json(err);
+    // render the error page
+    res.status(err.status || 500);
+    res.json(err);
 });
 
 module.exports = app;
