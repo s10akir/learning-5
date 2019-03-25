d6ae8a6d84c5bcf9f5dc8808cca0b5c2c853a634
Wataru Tsutsumi <k017c1055@it-neec.jp>
2019-03-25 10:14:35 +0900

    #96 @0.5h post、get受け取りのひな型の修正

--- routes/api.js
+++ routes/api.js
@@ -3,14 +3,18 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router();
-module.exports = router ;
+module.exports = router;
+
+// ボディーパーサー
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({extended: true}));
 
 router.use(express.static(path.join(__dirname, '../build')));
 
 // 掲示板への投稿データ受け取り
-router.post('/postMessage', function(req, res) {
-    // フォームの text1 という項目の取得
-    console.log(req.body);
+router.post('/post_message', function(req, res) {
+    // console.log(req.body.title);
+    // console.log(req.body.content);
 
     //エラー判定
     res.send(JSON.stringify({error: 'エラーコード'}));
@@ -18,8 +22,7 @@ router.post('/postMessage', function(req, res) {
 
 // アカウント登録 データ受け取り
 router.post('/register_account', function(req, res) {
-    // フォームの text1 という項目の取得
-    console.log(req.body.text1);
+    // console.log(req.body.text1);
 
     //エラー判定
     res.send(JSON.stringify({error: 'エラーコード'}));
@@ -27,16 +30,14 @@ router.post('/register_account', function(req, res) {
 
 // ログインデータ受け取り
 router.post('/login', function(req, res) {
-    // フォームの text1 という項目の取得
-    console.log(req.body.text1);
+    // console.log(req.body.text1);
 
     //エラー判定
     res.send(JSON.stringify({error: 'エラーコード'}));
 });
 
 router.get('/notice_boards', function(req, res) {
-    // フォームの text1 という項目の取得
-    console.log(req.body.text1);
+    // console.log(req.body.text1);
 
     //エラー判定
     res.send(JSON.stringify({data: 'データです'}));
